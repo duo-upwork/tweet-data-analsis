28ff6706b4c506acded2b6427a01119f4b85153f
paullaster <paullasterokoth98@gmail.com>
2022-11-27 15:57:20 +0300

    app settings

--- app.js
+++ app.js
@@ -10,13 +10,24 @@ import authOptions from './helpers/auth.twitter';
 //APP INSTANCES:
 const app = express();
 
+
+//APP SETTINGS:
+app.use ( express.json() );
+app.use ( express.urlencoded ( { extended: true } ));
+
 //ENVIRONMENTS:
 dotenv.config ();
 
 //TWITTER CLIENT:
 const twitterCLient = new TwitterAPI ( authOptions);
 
+
 //ENDPOINTS:
+app.get ( '/', ( req, res) => {
+    const twitterCLientData = twitterCLient.v2;
+    console.log ( twitterCLientData);
+    res.json ( twitterCLientData);
+})
 
 //START SERVER:
 app.listen (process.env.PORT, () => {
